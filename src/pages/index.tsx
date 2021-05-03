dc6d22d9abddb949123c386d9ca4074042edf326
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-03 15:08:03 -0300

    add initial folders and configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home Title</title>
+      </Head>
+
+      <main>
+        <p>Home</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
